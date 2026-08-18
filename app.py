f6829a105a09b4d44001db432c9c2824de9abe98
Arup <arupchakraborty7@gmail.com>
2026-08-18 06:55:55 +0530

    Add /vote/<name> and /results endpoints

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 
+votes = {} #in-memory store:{"Alice":3, "Bob":1}
+
 app = Flask(__name__)
 
 # Decorator
@@ -13,6 +15,16 @@ def home():
 
 def health():
     return "App is running"
+
+# Vote name and count
+@app.route("/vote/<name>")
+def vote(name):
+    votes[name] = votes.get(name, 0) + 1
+    return f"Vote recorded for {name}. Total votes: {votes[name]}"
+# Result on votes
+@app.route("/results")
+def results():
+    return jsonify(votes)
 
 if __name__ == "__main__":
     app.run(debug=True)
